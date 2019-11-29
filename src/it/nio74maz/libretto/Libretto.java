9ef02ed98745aba6e6c0bbaec15e740e1c1990d4
nio74 <mpaolo74@gmail.com>
2019-11-29 19:14:02 +0100

    completato punto 3

--- src/it/nio74maz/libretto/Libretto.java
+++ src/it/nio74maz/libretto/Libretto.java
@@ -37,4 +37,20 @@ public class Libretto {
         return result;
     }
 
+    /**
+     * Ricerca un {@link Voto} relativo al corso di cui è specificato il nome
+     *
+     * @param nomeEsame nome del corso da cercare
+     * @return il {@link Voto} corrispondente, oppure {@code null} se non esistente
+     */
+    public Voto cercaEsame(String nomeEsame){
+        for(Voto v : this.voti){
+            if (v.getCorso().equals(nomeEsame))
+            {
+                return v;
+            }
+        }
+        return  null;
+    }
+
 }
